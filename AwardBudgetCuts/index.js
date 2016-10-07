2c679e6a6c3f7ead02c545346f239a87cb4df4d5
bernard <bernard.igiri@gmail.com>
2016-10-06 20:41:08 -0400

    Fixed bug in Award Budget Cuts.

--- AwardBudgetCuts/index.js
+++ AwardBudgetCuts/index.js
@@ -5,22 +5,25 @@ function awardBudgetCuts(grants, budget) {
 		partialSums[i] = total;
 	}
 	var grantIndex = grants.length -1;
-	var previousIndex = grantIndex;
 	var cap = grants[grantIndex];
 	var proposedTotal = total;
 	while (proposedTotal > budget) {
 		var leftSum = partialSums[grantIndex-1];
 		var remainingGrants = grants.length - grantIndex;
-		if ((budget - leftSum) >= grants[grantIndex]) {
+		var currentGrant = grants[grantIndex];
+		var nextGrant = grants[grantIndex-1];
+		if ((budget - leftSum) >= currentGrant - nextGrant) {
 			cap = Math.floor((budget - leftSum)/remainingGrants);
 			proposedTotal = cap * remainingGrants + leftSum;
-			previousIndex = grantIndex;
 		}
 		grantIndex--;
-		if (grantIndex < 0) {
+		if (grantIndex === 0) {
 			cap = Math.floor(budget/grants.length);
 			break;
 		}
 	}
-	return cap;
+	return [cap,proposedTotal];
 }
+
+// 170
+// 100
